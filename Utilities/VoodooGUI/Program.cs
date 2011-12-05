a10088d1506408096f8c3bce2e599863642ad6bc
Sean Sube <peachykeen@voodooshader.com>
2011-12-05 04:02:26 -0600

    Finalized GUI CLI setup and interface. Incremented version to 0.5.2.

--- Utilities/VoodooGUI/Program.cs
+++ Utilities/VoodooGUI/Program.cs
@@ -38,32 +38,28 @@ namespace VoodooGUI
                 ConsoleVisible(Console.Title, true);
             }
 
-            Console.WriteLine("Voodoo Shader Framework GUI");
-            Console.WriteLine("version 0.5.0");
-            Console.WriteLine("Copyright (c) 2010-2011 by Sean Sube, All Rights Reserved. See COPYING for license details.");
-            Console.WriteLine("");
-
             // Command line mode
-            string help_cmd = null, run_hook = null, run_file = null;
-            bool verbose = false, help = false, update_self = false;
+            string run_hook = null, run_file = null;
+            bool verbose = false, help = false, version = false, logo = false, nologo = false, update_self = false;
             List<String> sync = new List<String>();
             List<String> install = new List<String>();
             List<String> update = new List<String>();
             List<String> remove = new List<String>();
             List<HookDef> hooks_add = new List<HookDef>();
 
-            var options = new OptionSet();
+            OptionSet options = new OptionSet();
             options.Add("v|verbose", "display detailed messages", v => { verbose = true; });
-            options.Add("h|?|help", "show this help and exit", v => { help = v != null; });
-            options.Add("c|command=", "show help for a specific {COMMAND}", v => help_cmd = v);
-            options.Add("s|sync=", "download manifests from the given {URI}", v => sync.Add(v));
-            options.Add("i|install=", "install the given {PACKAGE}", v => install.Add(v));
-            options.Add("u|update=", "updates the given {PACKAGE} and/or the gui", v => { if (v != null && v == "<self>") { update_self = true; } else { update.Add(v); }});
+            options.Add("n|no|nologo", "hide version info", v => nologo = v != null);
+            options.Add("version", "show the version info and exit", v => version = v != null);
+            options.Add("logo", "show the version info and exit", v => logo = v != null);
+            options.Add("h|?|help", "show help and exit", v => help = v != null);
+            options.Add("s|sync:", "download manifests from the given {URI}, or all listed remotes if none specified", v => sync.Add(v));
             options.Add("r|remove=", "removes the given {PACKAGE}", v => remove.Add(v));
-            options.Add("run=", "run the named {HOOK}, passing all extra arguments to the process", v => run_hook = v);
+            options.Add("i|install=", "install the given {PACKAGE}", v => install.Add(v));
+            options.Add("u|update:", "updates the given {PACKAGE} or the gui if {PACKAGE}='this', or all installed packages if none specified", 
+                v => { if (v != null && v == "this") { update_self = true; } else { update.Add(v); }});
+            options.Add("runhook=", "run the named {HOOK}, passing all extra arguments to the process", v => run_hook = v);
             options.Add("runfile=", "run the given {FILE}, passing all extra arguments to the process", v => run_file = v);
-            //options.Add("addhook=", "create a new hook with the given {TARGET}, {CONFIG} and {NAME}", (t, c, n) => hooks_add.Add({t, c, n}));
-            //options.Add("removehook=");
 
             List<String> extras;
             try
@@ -72,9 +68,35 @@ namespace VoodooGUI
             }
             catch (OptionException e)
             {
+                Console.WriteLine("Voodoo Shader Framework GUI");
                 Console.WriteLine("Arguments error: ");
                 Console.WriteLine("\t" + e.Message);
                 Console.WriteLine("Try `voodoo_ui --help` for more information.");
+                return;
+            }
+
+            if (version)
+            {
+                Console.WriteLine("VoodooGUI-0.5.0");
+                return;
+            }
+            else if (!nologo)
+            {
+                Console.WriteLine("Voodoo Shader Framework GUI");
+                Console.WriteLine("version 0.5.0");
+                Console.WriteLine("Copyright (c) 2010-2011 by Sean Sube, All Rights Reserved. See COPYING for license details.");
+                Console.WriteLine("");
+
+                if (logo)
+                {
+                    return;
+                }
+            }
+
+            if (help)
+            {
+                Console.WriteLine("Options:");
+                options.WriteOptionDescriptions(Console.Out);
                 return;
             }
 
@@ -102,6 +124,11 @@ namespace VoodooGUI
                 }
             }
 
+            if (update_self)
+            {
+                Console.WriteLine("Update GUI");
+            }
+
             if (update.Count > 0)
             {
                 foreach (String u in update)
@@ -110,10 +137,9 @@ namespace VoodooGUI
                 }
             }
 
-            if (help)
+            if (run_hook != null)
             {
-                PrintHelp(options, help_cmd);
-                return;
+                Console.WriteLine("Run Hook: {0}", run_hook);
             }
         }
 
@@ -123,60 +149,6 @@ namespace VoodooGUI
             if (hWnd != IntPtr.Zero)
             {
                 ShowWindow(hWnd, visible ? 1 : 0);
-            }
-        }
-
-        static void PrintHelp(OptionSet p, String command)
-        {
-            if (command == "sync")
-            {
-                Console.WriteLine("voodoo_ui.exe sync [remote [...]]");
-                Console.WriteLine("Downloads manifests from the given list, or known remotes if none are given.");
-            }
-            else if (command == "install")
-            {
-                Console.WriteLine("voodoo_ui.exe install [-v|--verbose] <package> [...]");
-                Console.WriteLine("Attempt to install the given packages. List may be name or PackId.");
-            }
-            else if (command == "update")
-            {
-                Console.WriteLine("voodoo_ui.exe update [-v|--verbose] [-self|<package> [...]]");
-                Console.WriteLine("Checks for and installs updates to all packages or the given list. List may be named or PackId.");
-                Console.WriteLine("\tIf -self is given, checks for GUI updates instead of packages.");
-            }
-            else if (command == "remove")
-            {
-                Console.WriteLine("voodoo_ui.exe remove [-v|--verbose] <package> [...]");
-                Console.WriteLine("Attempt to uninstall the given packages. List may be named or PackId.");
-            }
-            else if (command == "run")
-            {
-                Console.WriteLine("voodoo_ui.exe run [-s|--suspend] <name> [<args>]");
-                Console.WriteLine("Run the named hook's target. All arguments after the name are passed to the process.");
-                Console.WriteLine("\tIf -suspend is given, start the process suspended and forcibly inject the loader.");
-            }
-            else if (command == "runfile")
-            {
-                Console.WriteLine("voodoo_ui.exe run [-s|--suspend] <name> [<args>]");
-                Console.WriteLine("Run the given file. All arguments after the path are passed to the process.");
-                Console.WriteLine("\tIf -suspend is given, start the process suspended and forcibly inject the loader.");
-            }
-            else if (command == "addhook")
-            {
-                Console.WriteLine("voodoo_ui.exe addhook <target> [<config> [<name>]]");
-                Console.WriteLine("Create a new hook with the given target.");
-                Console.WriteLine("\tIf no config or name is given, a default will be searched for.");
-                Console.WriteLine("\tIf a target and config are given, the target will be used as the name.");
-            }
-            else if (command == "removehook ")
-            {
-                Console.WriteLine("voodoo_ui.exe removehook <name> [...]");
-                Console.WriteLine("Remove the named hooks.");
-            }
-            else
-            {
-                Console.WriteLine("Options:");
-                p.WriteOptionDescriptions(Console.Out);
             }
         }
     }
--- Utilities/VoodooNetClasses/Properties/AssemblyInfo.cs
+++ Utilities/VoodooNetClasses/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Voodoo .Net Classes")]
 [assembly: AssemblyDescription("Voodoo Shader Framework .Net Support Library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Sean Sube, et al.")]
 [assembly: AssemblyProduct("Voodoo Shader Framework")]
 [assembly: AssemblyCopyright("Copyright ©  2010-2011 by Sean Sube")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.5.2.0")]
+[assembly: AssemblyFileVersion("0.5.2.0")]
